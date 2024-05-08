84f994a04854f1ff42625c943c39e236e26f845d
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-08 17:29:09 +0200

    Add extract img elements functionality.

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 // - connect to https://memegen-link-examples-upleveled.netlify.app
 // -- fetch the html
 
-import https from 'https';
+import https from 'node:https';
+import { JSDOM } from 'jsdom';
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app';
 
 https
+  // Fetches html from url
   .get(url, (res) => {
-    // fetches html
     let data = '';
 
+    // Gets data in parts
     res.on('data', (chunk) => {
       data += chunk;
     });
 
+    // Processes data once response is finished
     res.on('end', () => {
-      console.log(data); // Shows fetched data (html)
+      // Parses data into a DOM
+      const dom = new JSDOM(data);
+      const document = dom.window.document;
+
+      // Selects all img elements
+      const imgElements = [...document.querySelectorAll('img')];
+      const imgSources = imgElements.map((img) => img.src);
+
+      console.log(imgSources); // Prints all img sources
+
+      // console.log(data); // Outputs fetched data (html)
     });
   })
   .on('error', (err) => {
